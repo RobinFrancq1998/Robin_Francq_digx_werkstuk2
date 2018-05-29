b24a738b9893b387a5a958582596cea0d5764453
RobinFrancq1998 <robin.francq@outlook.com>
2018-05-29 13:05:24 +0200

    afwerken van jsonparse

--- WebServiceTest/WebServiceTest/ViewController.swift
+++ WebServiceTest/WebServiceTest/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        parseURL(theURL: "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale")
+        
+        
+        /*
         // Initialisatie van de URL
         let url = URL(string: "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale")
         let urlRequest = URLRequest(url: url!)
@@ -38,19 +42,44 @@ class ViewController: UIViewController {
             
             do{
                 let json = try JSONSerialization.jsonObject(with: reponseData, options: [])as? [String: AnyObject]
+                print(json)
             } catch let error as NSError{
                 print ("Error while feshing ", error)
             }
-            
         }
         task.resume()
+        */
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func parseURL(theURL:String){
+        let url = URL(string: theURL)
+        URLSession.shared.dataTask(with: url!){(data, response, error) in
+            if error != nil {
+                print ("didn't work, \(String(describing: error))")
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() ){
+                    // ...
+                }
+            } else{
+                do{
+                    let parsedData = try JSONSerialization.jsonObject(with: data!) as! [[String:Any]]
+                    
+                    print(parsedData)
+                    
+                } catch let error as NSError{
+                    print(error)
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now()) {
+                        // ...
+                    }
+                }
+            }
+        }.resume()
+    }
 }
 
