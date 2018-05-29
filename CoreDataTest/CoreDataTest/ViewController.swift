07c62ee29db6a3f6202b2d1193755b5145a891ea
RobinFrancq1998 <robin.francq@outlook.com>
2018-05-29 10:48:12 +0200

    Aanmaken testProject

---
+++ CoreDataTest/CoreDataTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Robin Francq on 29/05/18.
+//  Copyright © 2018 Robin Francq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
